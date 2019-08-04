838cd5ff80982b724226504deb48b7bd054de564
Renée Cruz <reneebasscruz@gmail.com>
2019-08-04 19:46:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,6 @@ def run_guessing_game
   random_number = rand(6) + 1
   puts "Guess your own number between 1 and 6!"
   input = gets.chomp
-  random_number.to_s
   
   if input != random_number.to_s
     puts "Sorry! The computer guessed #{random_number}."
